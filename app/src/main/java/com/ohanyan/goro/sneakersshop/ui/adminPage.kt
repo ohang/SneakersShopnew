c16f008abb6466f92fc0d344999019aaf15e4eb4
Gor Ohanyan <ohangor@gmail.com>
2021-07-25 17:29:03 +0400

    25.07

---
+++ app/src/main/java/com/ohanyan/goro/sneakersshop/ui/adminPage.kt
@@ -0,0 +1,21 @@
+package com.ohanyan.goro.sneakersshop.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ohanyan.goro.sneakersshop.R
+
+class adminPage: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_admin_page, container, false)
+
+        return view
+
+    }
+}
